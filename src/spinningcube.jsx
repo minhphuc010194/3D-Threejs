61ba45af6db17cc67b20c159ba02f98b814631c7
bum <bum@git.com>
2020-10-22 16:09:19 +0700

    khoi vuong xoay

---
+++ src/spinningcube.jsx
@@ -0,0 +1,31 @@
+import React,{useRef, useEffect} from 'react';
+import * as THREE from 'three';
+// import three from 'three';
+
+export default function SpinningCube(props) {
+    const {width, height} = props;
+    const anchor = useRef("anchor");
+
+    useEffect(()=>{
+        const scene = new THREE.Scene(),
+            camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000),
+            renderer = new THREE.WebGLRenderer();
+        renderer.setSize(width, height);
+        anchor.current.appendChild(renderer.domElement);
+        const geometry = new THREE.BoxGeometry(1,1,1),
+              material = new THREE.MeshBasicMaterial({color: 0x00ff00}),
+              cube = new THREE.Mesh(geometry, material);
+              scene.add(cube);
+              camera.position.z = 5;
+              function gameLoop(){
+                  requestAnimationFrame(gameLoop);
+                  cube.rotation.x += 0.03;
+                  cube.rotation.y += 0.03;
+                  renderer.render(scene, camera);
+              }
+              gameLoop();
+    },[])
+    return (
+        <div ref={anchor} style={{width, height, margin:"0 auto"}} />
+    )
+}
